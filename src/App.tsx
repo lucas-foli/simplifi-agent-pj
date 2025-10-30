ef8455fad6f45817346af6610572c6e04161773c
lucas-foli <lucas.defoliveira@gmail.com>
2025-10-30 16:05:01 -0300

    fix: route landing ctAs to auth gateway

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import AuthGateway from "./pages/AuthGateway";
 import Login from "./pages/Login";
 import Onboarding from "./pages/Onboarding";
 import Dashboard from "./pages/Dashboard";
